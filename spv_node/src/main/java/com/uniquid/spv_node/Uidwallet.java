4b80b206b6b6dd2dc403d6af82e8abf26609a3b7
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-01-19 18:41:48 +0100

    and finally it signs!!!!

--- spv_node/src/main/java/com/uniquid/spv_node/Uidwallet.java
+++ spv_node/src/main/java/com/uniquid/spv_node/Uidwallet.java
@@ -1,33 +1,20 @@
 package com.uniquid.spv_node;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
 import java.util.List;
 
 import org.bitcoinj.core.Context;
 import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.core.ScriptException;
 import org.bitcoinj.core.Transaction;
 import org.bitcoinj.core.TransactionInput;
 import org.bitcoinj.core.TransactionOutput;
-import org.bitcoinj.script.Script;
-import org.bitcoinj.signers.MissingSigResolutionSigner;
-import org.bitcoinj.signers.TransactionSigner;
-import org.bitcoinj.wallet.DecryptingKeyBag;
-import org.bitcoinj.wallet.KeyBag;
 import org.bitcoinj.wallet.KeyChainGroup;
-import org.bitcoinj.wallet.RedeemData;
 import org.bitcoinj.wallet.SendRequest;
 import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.Wallet.MissingSigsMode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Uidwallet extends Wallet {
 	
-	private static final Logger log = LoggerFactory.getLogger(Wallet.class);
-
 	public Uidwallet(NetworkParameters params) {
 		super(params);
 	}
@@ -40,72 +27,67 @@ public class Uidwallet extends Wallet {
 		super(params, keyChainGroup);
 	}
 
-	public void peppeSignTransaction(SendRequest req) {
-
+	public void completeTransaction(SendRequest sendRequest) throws Exception {
 		lock.lock();
 		try {
-			Transaction tx = req.tx;
+			
+			Transaction tx = sendRequest.tx;
 			List<TransactionInput> inputs = tx.getInputs();
-			List<TransactionOutput> outputs = tx.getOutputs();
 			checkState(inputs.size() > 0);
-			checkState(outputs.size() > 0);
-
-			KeyBag maybeDecryptingKeyBag = new DecryptingKeyBag(this, req.aesKey);
-
+			
 			int numInputs = tx.getInputs().size();
 			for (int i = 0; i < numInputs; i++) {
 				TransactionInput txIn = tx.getInput(i);
 				if (txIn.getConnectedOutput() == null) {
-					// l'input non ci sta.
-					// cerchiamolo nel wallet
+					// no connected input linked. We need to search if a transaction is present in wallet
 
 					List<TransactionOutput> candidates = calculateAllSpendCandidates(true,
-							req.missingSigsMode == MissingSigsMode.THROW);
-
-					for (TransactionOutput outCandidate : candidates) {
-
-						txIn.connect(outCandidate.getOutPointFor().getConnectedOutput());
+							sendRequest.missingSigsMode == MissingSigsMode.THROW);
+					
+					TransactionOutput cloned = connectedTxOut(candidates, txIn);
+					
+					if (cloned != null) {
+						txIn.connect(cloned);
 					}
-
-					break;
+					
 				}
-
-				try {
-					// We assume if its already signed, its hopefully got a
-					// SIGHASH type that will not invalidate when
-					// we sign missing pieces (to check this would require
-					// either assuming any signatures are signing
-					// standard output types or a way to get processed
-					// signatures out of script execution)
-					txIn.getScriptSig().correctlySpends(tx, i, txIn.getConnectedOutput().getScriptPubKey());
-					log.warn(
-							"Input {} already correctly spends output, assuming SIGHASH type used will be safe and skipping signing.",
-							i);
-					continue;
-				} catch (ScriptException e) {
-					log.debug("Input contained an incorrect signature", e);
-					// Expected.
-				}
-
-				Script scriptPubKey = txIn.getConnectedOutput().getScriptPubKey();
-				RedeemData redeemData = txIn.getConnectedRedeemData(maybeDecryptingKeyBag);
-				checkNotNull(redeemData, "Transaction exists in wallet that we cannot redeem: %s",
-						txIn.getOutpoint().getHash());
-				txIn.setScriptSig(scriptPubKey.createEmptyInputScript(redeemData.keys.get(0), redeemData.redeemScript));
+			
 			}
-
-			TransactionSigner.ProposedTransaction proposal = new TransactionSigner.ProposedTransaction(tx);
-			for (TransactionSigner signer : signers) {
-				if (!signer.signInputs(proposal, maybeDecryptingKeyBag))
-					log.info("{} returned false for the tx", signer.getClass().getName());
-			}
-
-			// resolve missing sigs if any
-			new MissingSigResolutionSigner(req.missingSigsMode).signInputs(proposal, maybeDecryptingKeyBag);
+			
 		} finally {
 			lock.unlock();
 		}
+	}
+	
+	private TransactionOutput connectedTxOut(List<TransactionOutput> candidates, TransactionInput txIn) {
+		
+		for (TransactionOutput outCandidate : candidates) {
+			
+			if (outCandidate.getOutPointFor().getHash().equals(txIn.getOutpoint().getHash())) {
+				
+				return cloneTx(outCandidate);
+				
+			}
+			
+		}
+		
+		return null;
+		
+	}
+	
+	
+	private TransactionOutput cloneTx(TransactionOutput t) {
+		
+		Transaction original = t.getParentTransaction();
+		
+		byte[] tSerialized = original.bitcoinSerialize();
+		
+		// Make a previous tx simply to send us sufficient coins. This prev tx is not really valid but it doesn't
+	    // matter for our purposes.
+	    Transaction prevTx = new Transaction(params, tSerialized);
 
+	    	return prevTx.getOutput(0);
+	    
 	}
 
 }
