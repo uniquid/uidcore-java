8b67e912a97bac72990ea3ed3cc22259b556bddb
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-02-14 11:37:07 +0100

    fix a problem with JSON libraries in Android (don't provide toMap)

--- core/src/main/java/com/uniquid/core/connector/mqtt/JSONMessage.java
+++ core/src/main/java/com/uniquid/core/connector/mqtt/JSONMessage.java
@@ -1,9 +1,12 @@
 package com.uniquid.core.connector.mqtt;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -89,10 +92,38 @@ public class JSONMessage {
 
 		JSONObject jsonBody = jsonMessage.getJSONObject("body");
 
-		message.setBody(jsonBody.toMap());
+		message.setBody(toMap(jsonBody));
 
 		return message;
 
 	}
+	
+	private static Map<String, Object> toMap(JSONObject object) throws JSONException {
+        Map<String, Object> map = new HashMap();
+        Iterator keys = object.keys();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            map.put(key, fromJson(object.get(key)));
+        }
+        return map;
+    }
+
+	private static List toList(JSONArray array) throws JSONException {
+        List list = new ArrayList();
+        for (int i = 0; i < array.length(); i++) {
+            list.add(fromJson(array.get(i)));
+        }
+        return list;
+    }
+
+	private static Object fromJson(Object json) throws JSONException {
+        if (json instanceof JSONObject) {
+            return toMap((JSONObject) json);
+        } else if (json instanceof JSONArray) {
+            return toList((JSONArray) json);
+        } else {
+            return json;
+        }
+    }
 
 }
