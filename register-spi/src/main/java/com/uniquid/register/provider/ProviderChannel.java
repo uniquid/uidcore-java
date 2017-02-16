c8409a16bfb5d6234d8c09397251627756bf7590
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-02-16 13:14:37 +0100

    improved ProviderChannel

--- register-spi/src/main/java/com/uniquid/register/provider/ProviderChannel.java
+++ register-spi/src/main/java/com/uniquid/register/provider/ProviderChannel.java
@@ -1,9 +1,17 @@
 package com.uniquid.register.provider;
 
+import java.io.Serializable;
+
 /**
  * Created by Beatrice Formai on 03/01/17, for Uniquid Inc.
  */
-public class ProviderChannel {
+public class ProviderChannel implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private String providerAddress;
 	private String userAddress;
 	private String revokeAddress;
@@ -64,5 +72,26 @@ public class ProviderChannel {
 		return "provider address: " + providerAddress + "; user address: " + userAddress + "; bitmask: " + bitmask +
 				"; revoke address: " + revokeAddress + "; revokeTxId: " + revokeTxId;
 	}
-	
+
+	@Override
+	public boolean equals(Object object){
+		if(!(object instanceof ProviderChannel)) return false;
+
+		ProviderChannel providerChannel = (ProviderChannel) object;
+		return (providerAddress.equals(providerChannel.getProviderAddress()) &&
+				userAddress.equals(providerChannel.getUserAddress())
+		);
+	}
+
+	@Override
+	public int hashCode(){
+		int result = 7;
+		result = 31 * result + (providerAddress != null ? providerAddress.hashCode() : 0);
+		result = 31 * result + (userAddress != null ? userAddress.hashCode() : 0);
+		result = 31 * result + (bitmask != null ? bitmask.hashCode() : 0);
+		result = 31 * result + (revokeAddress != null ? revokeAddress.hashCode() : 0);
+		result = 31 * result + (revokeTxId != null ? revokeTxId.hashCode() : 0);
+		return result;
+	}
+
 }
