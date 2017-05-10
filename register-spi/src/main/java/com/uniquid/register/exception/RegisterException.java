6ee9597efe291906cd48f0bcb34d09bc804019a9
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-05-10 12:42:41 +0200

    improve javadoc

--- register-spi/src/main/java/com/uniquid/register/exception/RegisterException.java
+++ register-spi/src/main/java/com/uniquid/register/exception/RegisterException.java
@@ -1,18 +1,40 @@
 package com.uniquid.register.exception;
 
 /**
- * Created by beatriz on 03/01/17, for Uniquid Inc.
+ * Signal that a problem accessing the data source occurred 
  */
-public class RegisterException extends  Exception {
+public class RegisterException extends Exception {
 
-    public RegisterException(String message) {
+	private static final long serialVersionUID = 1L;
+
+	/**
+     * Constructs a new register exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param   message   the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
+	public RegisterException(String message) {
         super(message);
     }
 
+	/**
+     * Constructs a new register exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this exception's detail message.
+     *
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link #getMessage()} method).
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     * @since  1.4
+     */
     public RegisterException(String message, Throwable cause) {
         super(message, cause);
     }
-
-
 
 }
