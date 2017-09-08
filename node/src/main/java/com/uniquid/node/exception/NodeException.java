470e24ef2243b820e2a4b7f8017e51e19a61bbd0
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-09-08 15:54:56 +0200

    removed empty constructor

--- node/src/main/java/com/uniquid/node/exception/NodeException.java
+++ node/src/main/java/com/uniquid/node/exception/NodeException.java
@@ -1,38 +1,36 @@
 package com.uniquid.node.exception;
 
 /**
- * Signal that a problem inside the Uniquid Node occurred 
+ * Signal that a problem inside the Uniquid Node occurred
  */
 public class NodeException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Constructs a new exception with null as its detail message
-	 */
-	public NodeException() {
-        super();
-    }
-
-	/**
 	 * Constructs a new exception with the specified detail message
 	 * 
-     * @param message the detail message (which is saved for later retrieval by the getMessage() 
-	 method).
+	 * @param message
+	 *            the detail message (which is saved for later retrieval by the
+	 *            getMessage() method).
 	 */
-    public NodeException(String message) {
-        super(message);
-    }
+	public NodeException(String message) {
+		super(message);
+	}
 
-    /**
-     * Constructs a new exception with null as its detail message
-     * @param message the detail message (which is saved for later retrieval by the getMessage() 
-	 method).
-     * @param cause the cause (which is saved for later retrieval by the getCause() method). (A null 
-	 value is permitted, and indicates that the cause is nonexistent or unknown.)
-     */
-    public NodeException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	/**
+	 * Constructs a new exception with null as its detail message
+	 * 
+	 * @param message
+	 *            the detail message (which is saved for later retrieval by the
+	 *            getMessage() method).
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the
+	 *            getCause() method). (A null value is permitted, and indicates
+	 *            that the cause is nonexistent or unknown.)
+	 */
+	public NodeException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
 }
