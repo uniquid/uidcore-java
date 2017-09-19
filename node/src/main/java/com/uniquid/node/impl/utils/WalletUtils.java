b939ffbde12d0e30d52ad4ac816e3b067a5da229
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-09-19 13:04:07 +0200

    remove dead code

--- node/src/main/java/com/uniquid/node/impl/utils/WalletUtils.java
+++ node/src/main/java/com/uniquid/node/impl/utils/WalletUtils.java
@@ -1,25 +1,10 @@
 package com.uniquid.node.impl.utils;
 
-import static com.google.common.base.Preconditions.checkState;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
-import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Transaction;
-import org.bitcoinj.core.TransactionInput;
 import org.bitcoinj.core.TransactionOutput;
 import org.bitcoinj.script.Script;
-import org.bitcoinj.wallet.SendRequest;
-import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.Wallet.MissingSigsMode;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongycastle.util.encoders.Hex;
@@ -30,80 +15,6 @@ import org.spongycastle.util.encoders.Hex;
 public abstract class WalletUtils {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(WalletUtils.class.getName());
-	
-    /**
-     * Allow to complete a deserialized transaction from content fetched from the wallet
-     * @param sendRequest
-     * @param wallet
-     * @param params
-     * @throws Exception
-     */
-	public static void newCompleteTransaction(SendRequest sendRequest, Wallet wallet, NetworkParameters params) throws Exception {
-
-		Transaction tx = sendRequest.tx;
-		List<TransactionInput> inputs = tx.getInputs();
-		checkState(inputs.size() > 0);
-		
-		int numInputs = tx.getInputs().size();
-		for (int i = 0; i < numInputs; i++) {
-			TransactionInput txIn = tx.getInput(i);
-			if (txIn.getConnectedOutput() == null) {
-				// no connected input linked. We need to search if a transaction is present in wallet
-
-				List<TransactionOutput> candidates = wallet.calculateAllSpendCandidates(true,
-						sendRequest.missingSigsMode == MissingSigsMode.THROW);
-				
-				TransactionOutput cloned = connectedTxOut(candidates, txIn, params);
-				
-				if (cloned != null) {
-					txIn.connect(cloned);
-				}
-				
-			}
-		
-		}
-	}
-	
-	private static TransactionOutput connectedTxOut(List<TransactionOutput> candidates, TransactionInput txIn, NetworkParameters params) {
-		
-		for (TransactionOutput outCandidate : candidates) {
-			
-			if (outCandidate.getOutPointFor().getHash().equals(txIn.getOutpoint().getHash())) {
-				
-				return cloneTx(outCandidate, params);
-				
-			}
-			
-		}
-		
-		return null;
-		
-	}
-	
-	/**
-	 * Return a copy of the transaction output
-	 * @param t transaction output to clone
-	 * @param params the {@link NetworkParameters} to use
-	 * @return a clone of the transaction output
-	 */
-	public static TransactionOutput cloneTx(TransactionOutput t, NetworkParameters params) {
-		
-		Transaction original = t.getParentTransaction();
-		
-		byte[] tSerialized = original.bitcoinSerialize();
-		
-		// Make a previous tx simply to send us sufficient coins. This prev tx is not really valid but it doesn't
-	    // matter for our purposes.
-	    Transaction prevTx = new Transaction(params, tSerialized);
-
-	    	return prevTx.getOutput(0);
-	    
-	}
-	
-//	public static boolean hasTransaction(String txid, Wallet wallet) {
-//		// NativeSecp256k1.schnorrSign();
-//		return wallet.getTransaction(Sha256Hash.of(txid.getBytes())) != null;
-//	}
 	
 	/**
      * Check if a Transaction have a valid (Uniquid) OP_RETURN
@@ -141,56 +52,6 @@ public abstract class WalletUtils {
         
     }
 	
-//    public static boolean isUnspent(String txid, String address){
-//        String result = httpGet(URL_UTXO, address);
-//
-//        if(result == null)
-//            return false;
-//
-//        JSONArray jArray = new JSONArray(result);
-//
-//        for(int i = 0; i < jArray.length(); i++){
-//            JSONObject jsonObject = jArray.getJSONObject(i);
-//            if(jsonObject.getString("txid").equals(txid) && jsonObject.getInt("vout") == 2)
-//                return true;
-//        }
-//
-//        return false;
-//    }
-    
-    public static String httpGet(String url, String param){
-    	try {
-            HttpURLConnection connection;
-            if(param != null){
-                connection = (HttpURLConnection) new URL(url.replace("%1&s", param)).openConnection();
-            } else {
-                connection = (HttpURLConnection) new URL(url).openConnection();
-            }
-            connection.setRequestMethod("GET");
-            int responseCode = connection.getResponseCode();
-            if(responseCode == 200) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-                LOGGER.info("HTTPGET_RESP: " + response.toString());
-                return response.toString();
-            }
-        } catch (java.net.ProtocolException e) {
-        		LOGGER.error("Exception", e);
-        } catch (MalformedURLException e) {
-        		LOGGER.error("Exception", e);
-        } catch (IOException e) {
-        		LOGGER.error("Exception", e);
-        }
-
-        return null;
-    }
-
     /**
      * Retrieve OP_RETURN from a Transaction
      * */
--- node/src/test/java/com/uniquid/node/impl/utils/WalletUtilsTest.java
+++ node/src/test/java/com/uniquid/node/impl/utils/WalletUtilsTest.java
@@ -1,7 +1,6 @@
 package com.uniquid.node.impl.utils;
 
 import org.bitcoinj.core.Transaction;
-import org.bitcoinj.core.TransactionOutput;
 import org.junit.Assert;
 import org.junit.Test;
 import org.spongycastle.util.encoders.Hex;
@@ -9,19 +8,6 @@ import org.spongycastle.util.encoders.Hex;
 import com.uniquid.node.impl.params.UniquidRegTest;
 
 public class WalletUtilsTest {
-	
-	@Test
-	public void testCloneTx() {
-		
-		String imprinting = "0100000001f9b665bb7e333ae1a36ea3a0b54478f5f13cf4779a5c5e0ff2acbb4d8dbf1db5010000006a47304402202b28f031a63a3e5107a05bb09cf8a12be3a5d659b7bdaef2150f76f0837e39c7022015bfcde7475ceedf556061811b1bcc18290e9fff73195074142ecdc4ed15d8c3012103fd0f9cc115ebf8fa49d8915c0c0cee926d3a6a67754849da230639f0384d25c9ffffffff0240420f00000000001976a914856ef40e30d6f46afb403a46104dbcea53729e7088ace0c35b05000000001976a9149723c240599c36a9dd5a3d3473dbb0bd7875f56a88ac00000000";
-		
-		Transaction originalTransaction = UniquidRegTest.get().getDefaultSerializer().makeTransaction(Hex.decode(imprinting));
-		
-		TransactionOutput cloned = WalletUtils.cloneTx(originalTransaction.getOutput(0), UniquidRegTest.get());
-		
-		Assert.assertEquals(originalTransaction.getOutput(0).toString(), cloned.toString());
-		
-	}
 	
 	@Test
 	public void testOpReturn() {
@@ -36,12 +22,4 @@ public class WalletUtilsTest {
 		
 	}
 	
-	@Test
-	public void testHttpGet() {
-		String response = WalletUtils.httpGet("http://www.google.com:80", null);
-		
-		Assert.assertNotNull(response);
-		
-	}
-
 }
