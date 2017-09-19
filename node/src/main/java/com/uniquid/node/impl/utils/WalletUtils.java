a46f8e67c86eaab5d3a63404d7fc0695f2fbe547
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-09-19 14:55:29 +0200

    improve logic to decode opreturn and unit tested

--- node/src/main/java/com/uniquid/node/impl/utils/WalletUtils.java
+++ node/src/main/java/com/uniquid/node/impl/utils/WalletUtils.java
@@ -1,5 +1,7 @@
 package com.uniquid.node.impl.utils;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bitcoinj.core.Transaction;
@@ -7,76 +9,89 @@ import org.bitcoinj.core.TransactionOutput;
 import org.bitcoinj.script.Script;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.spongycastle.util.encoders.Hex;
 
 /**
  * Utility class that contains useful methods for managing wallets
  */
 public abstract class WalletUtils {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(WalletUtils.class.getName());
-	
-	/**
-     * Check if a Transaction have a valid (Uniquid) OP_RETURN
-     * */
-	public static boolean isValidOpReturn(Transaction tx){
-        String op_return = getOpReturn(tx);
-        
-        if (op_return != null) {
 
-        	return Hex.decode(op_return).length == 80;
+	private static final int OP_RETURN_INDEX = 1;
 
-        } else {
-        	
-        	return false;
-        
-        }
-    }
-	
 	/**
-     * Check if a Transaction have a valid (Uniquid) OP_RETURN
-     * */
-	public static boolean isValidOpReturn(TransactionOutput to){
+	 * Check if a Transaction has a valid (Uniquid) OP_RETURN
+	 */
+	public static boolean isValidOpReturn(Transaction tx) throws Exception {
+
+		byte[] opreturn = getOpReturnAsByteArray(tx);
+
+		if (opreturn != null && opreturn.length == 80) {
+
+			return true;
+
+		} else {
+
+			return false;
+
+		}
+	}
+
+	/**
+	 * Retrieve OP_RETURN from a Transaction as Hex String
+	 */
+	public static String getOpReturn(Transaction tx) throws Exception {
+
+		byte[] opreturn = getOpReturnAsByteArray(tx);
+
+		if (opreturn != null) {
+
+			return org.bitcoinj.core.Utils.HEX.encode(opreturn);
+
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * Retrieve OP_RETURN from a Transaction as byte array
+	 * @param tx
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] getOpReturnAsByteArray(Transaction tx) throws Exception {
+
+		List<TransactionOutput> to = tx.getOutputs();
+
+		if (to.size() == 4) {
+
+			Script script = to.get(OP_RETURN_INDEX).getScriptPubKey();
+
+			if (script.isOpReturn()) {
+
+				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+				script.getChunks().get(1).write(byteArrayOutputStream);
+
+				// FIRST BYTE IS OPCODE
+				// SECOND BYTE IS TOTAL LENGHT OF OPRETURN
+				// THIRD BYTE IS FIRST ELEMENT OP OPRETURN
+
+				byte[] opreturn = byteArrayOutputStream.toByteArray();
+
+				return Arrays.copyOfRange(opreturn, 2, opreturn[1] + 2);
+
+			}
+
+			return null;
+
+		} else {
+
+			return null;
+
+		}
 		
-		String op_return = getOpReturn(to);
-        
-        if (op_return != null) {
+	}
 
-        	return Hex.decode(op_return).length == 80;
-
-        } else {
-        	
-        	return false;
-        
-        }
-        
-    }
-	
-    /**
-     * Retrieve OP_RETURN from a Transaction
-     * */
-    public static String getOpReturn(Transaction tx){
-        List<TransactionOutput> to = tx.getOutputs();
-        Script script = to.get(1).getScriptPubKey();
-        if(script.isOpReturn()){
-            String script_string = script.toString();
-            return script_string.substring(script_string.indexOf("[")+1, script_string.indexOf("]"));
-        }
-        return null;
-    }
-    
-    public static String getOpReturn(TransactionOutput to) {
-    	
-        Script script = to.getScriptPubKey();
-        
-        if (script.isOpReturn()) {
-        	
-            String script_string = script.toString();
-            return script_string.substring(script_string.indexOf("[")+1, script_string.indexOf("]"));
-            
-        }
-        
-        return null;
-    }
-	
 }
