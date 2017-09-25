45840e56a553ecb8484d9e8d07019714aa598abf
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-09-25 11:42:30 +0200

    allow to receive userclientfactory as configuration

--- node/src/main/java/com/uniquid/node/impl/UniquidWatchingNodeImpl.java
+++ node/src/main/java/com/uniquid/node/impl/UniquidWatchingNodeImpl.java
@@ -36,6 +36,7 @@ import com.uniquid.node.impl.utils.NodeUtils;
 import com.uniquid.node.listeners.UniquidNodeEventListener;
 import com.uniquid.register.RegisterFactory;
 import com.uniquid.register.provider.ProviderChannel;
+import com.uniquid.userclient.UserClientFactory;
 
 /**
  * Implementation of an Uniquid Node with BitcoinJ library
@@ -466,6 +467,12 @@ public class UniquidWatchingNodeImpl<T extends UniquidNodeConfiguration> impleme
 		@SuppressWarnings("unchecked")
 		public B setRegistryUrl(String registryUrl) {
 			_uniquidNodeConfiguration.setRegistryUrl(registryUrl);
+			return (B) this;
+		}
+		
+		@SuppressWarnings("unchecked")
+		public B setUserClientFactory(UserClientFactory userClientFactory) {
+			_uniquidNodeConfiguration.setUserClientFactory(userClientFactory);
 			return (B) this;
 		}
 
